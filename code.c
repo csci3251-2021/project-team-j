dce68f3af0c516249b29ae7be1b0a24753d0eb10
iscoco23 <79909277+iscoco23@users.noreply.github.com>
2021-05-01 17:28:18 +0800

    Create code.c
    simple hello world c code for task 6 (workflow)
    feel free to edit

--- code.c
+++ code.c
@@ -1,1 +1,11 @@
 // Your C code here
+// simple hello world c code by Kwok Chun Yin 1155144588
+// for testing purpose (task 6 workflow)
+// feel free to edit
+#include <stdio.h>
+
+int main()
+{
+  printf("Hello world");
+  return 0;
+}
